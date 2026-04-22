5c7bd9b322cdfbdb7cab4c565a398a1c2253d3ad
jacksama11223 <jacktayden@gmail.com>
2026-04-22 13:24:19 +0700

    web services day du hon

--- SmartLMS.Data/obj/Debug/net8.0/SmartLMS.Data.AssemblyInfo.cs
+++ SmartLMS.Data/obj/Debug/net8.0/SmartLMS.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartLMS.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c742027640c3460b40430e6b0d2c58979649e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22fc01f477cf6cff026ee92c46e0aa96c2699de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartLMS.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartLMS.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SmartLMS.Models/obj/Debug/net8.0/SmartLMS.Models.AssemblyInfo.cs
+++ SmartLMS.Models/obj/Debug/net8.0/SmartLMS.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartLMS.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c742027640c3460b40430e6b0d2c58979649e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22fc01f477cf6cff026ee92c46e0aa96c2699de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartLMS.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartLMS.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
